d8b029cea712bac0a5d1b3eb99223469ca5baedd
Mike Coopman <macoopman@gmail.com>
2018-03-14 22:49:13 -0700

    version 1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
-/*
- * Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number
- * and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
- *
- * multiples of 3 print "Fizz"
- * multiples of 5 print "Buzz"
- * multiples of 3 and 5 print "FizzBuzz"
- */
-
-
-
 
 int main() {
+    string fizz = "Fizz";
+    string buzz = "Buzz";
 
-    
+    for(int i = 1; i <= 100; i++){
+        if(i % 3 == 0 && i % 5 == 0)
+            printf("%s%s\n",fizz.c_str(), buzz.c_str());
+        else if(i % 3 == 0 && i % 5 != 0){
+            printf("%s\n",fizz.c_str());
+        }
+        else if(i % 5 == 0 && i % 3 != 0){
+            printf("%s\n",buzz.c_str());
+        }
+        else
+            printf("%d\n",i);
+    }
 
     return 0;
 }
