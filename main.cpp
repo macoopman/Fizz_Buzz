e6956e16581f01a633397425f1ac34b221b2da62
Mike Coopman <macoopman@gmail.com>
2018-03-14 22:56:10 -0700

    Version 2

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main() {
     for(int i = 1; i <= 100; i++){
         if(i % 3 == 0 && i % 5 == 0)
             printf("%s%s\n",fizz.c_str(), buzz.c_str());
-        else if(i % 3 == 0 && i % 5 != 0){
+        else if(i % 3 == 0){
             printf("%s\n",fizz.c_str());
         }
-        else if(i % 5 == 0 && i % 3 != 0){
+        else if(i % 5 == 0){
             printf("%s\n",buzz.c_str());
         }
         else
