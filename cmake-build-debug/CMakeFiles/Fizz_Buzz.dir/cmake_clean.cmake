b3ffb9ef34aec1995570938b943d0231e68de197
Mike Coopman <macoopman@gmail.com>
2018-03-14 22:41:19 -0700

    first commit

---
+++ cmake-build-debug/CMakeFiles/Fizz_Buzz.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fizz_Buzz.dir/main.cpp.o"
+  "Fizz_Buzz.pdb"
+  "Fizz_Buzz"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fizz_Buzz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+ * Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number
+ * and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+ *
+ * multiples of 3 print "Fizz"
+ * multiples of 5 print "Buzz"
+ * multiples of 3 and 5 print "FizzBuzz"
+ */
+
+
+
+
+int main() {
+
+    
+
+    return 0;
+}
